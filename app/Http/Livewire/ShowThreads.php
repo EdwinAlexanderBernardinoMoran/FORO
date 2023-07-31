0ae85935bc1eb3fbaf1e2d8f36d633e22172135e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-31 15:30:42 -0600

    Estructura visual de las preguntas, modificando controlador ShowThreads

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Category;
+use App\Models\Thread;
 use Livewire\Component;
 
 class ShowThreads extends Component
@@ -10,8 +11,11 @@ class ShowThreads extends Component
     public function render()
     {
         $categories = Category::get();
+        $threads = Thread::latest()->get();
+
         return view('livewire.show-threads', [
-            'categories' => $categories
+            'categories' => $categories,
+            'threads' => $threads
         ]);
     }
 }
